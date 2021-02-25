2c8e292a509328c9fbaf88a4f9859271f49bb25a
cadet jean <72902297+CadetJean@users.noreply.github.com>
2021-02-25 01:58:28 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.Write("enter a number: ");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("enter a number: ");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("enter operand: ");
+            char operand = Convert.ToChar(Console.ReadLine());
+          
+            switch (operand)
+            {
+
+                // If user enter +
+                case '+':
+                    Console.Write(numOne + numTwo);
+                    break;
+
+                // If user enter -
+                case '-':
+                    Console.Write(numOne - numTwo);
+                    break;
+
+                // If user enter *
+                case '*':
+                    Console.Write(numOne * numTwo);
+                    break;
+
+                // If user enter /
+                case '/':
+                    Console.Write(numOne / numTwo);
+                    break;
+
+                default:
+                    Console.WriteLine("Error! operator is not correct");
+                    break;
+
+            }
+
+            Console.ReadLine();
+        
+
+        }
+    }
+}
